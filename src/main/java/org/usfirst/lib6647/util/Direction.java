1636415edacb6701fc3393e5583e3718ae2b5ddb
pacoito123 <pacoito@protonmail.com>
2019-06-28 12:25:01 -0500

    Moved lib6647 to its own repository. [General] - Removed lib6647 files. - Added lib6647 gradle dependency.

--- src/main/java/org/usfirst/lib6647/util/Direction.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.lib6647.util;
-
-/**
- * Enum listing possible movement directions, for readability purposes.
- */
-public enum Direction {
-    UP, DOWN, FRONT, BACK, LEFT, RIGHT, FORWARD, BACKWARD, IN, OUT, NONE
-}
